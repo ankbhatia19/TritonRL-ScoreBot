0d82529c5fec81e7f2c75ead778b5a73f1217a43
rileyfroehlich <rileyfro2@gmail.com>
2021-02-17 17:40:33 -0800

    Local card functional

---
+++ resources/fonts.py
@@ -0,0 +1,27 @@
+DINProCond = "assets/19/fonts/DINPro-Cond.otf"
+DINProCondMedium = "assets/19/fonts/DINPro-CondMedium.otf"
+DINProCondBold = "assets/19/fonts/DINPro-CondBold.otf"
+
+ChampionsRegular = "assets/19/fonts/Champions-Regular.otf"
+ChampionsBold = "assets/19/fonts/Champions-Bold.ttf"
+
+
+'''
+FONTS_FORMAT = ((Overall Font, size)
+                (Position Font, size)
+                (Name Font, size, size))
+                (Attribute value Font, size) Font, size)
+                (Attribute label Font, size))
+'''
+
+dinpro_fonts = ((DINProCondMedium, 120),
+                (DINProCond, 50),
+                (DINProCondBold, 70),
+                (DINProCondBold, 55),
+                (DINProCond, 50))
+
+champions_fonts = ((ChampionsRegular, 115),
+                   (ChampionsRegular, 50),
+                   (ChampionsRegular, 68),
+                   (ChampionsRegular, 55),
+                   (ChampionsRegular, 50))
